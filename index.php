6558c0045fb64fe4aaabe04342e7bec998ec9862
jad amine <jade1992amine@gmail.com>
2022-05-27 21:08:50 +0300

    Displayed Pizzas with Materialize

--- index.php
+++ index.php
@@ -3,15 +3,20 @@
  // connect to db
  $conn = mysqli_connect('localhost', 'jad', 'test123', 'pizza');
 
- $sql = 'select * from pizzas';
+ // write query for all piza
+ $sql = 'select * from pizzas ORDER By created_at';
 
+ // make query & get result as an row object
  $result = mysqli_query($conn, $sql);
 
+ // fetch the resulting rows as an array
  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-foreach($pizzas as $pizza){
-   echo $pizza['title'] . '<br>';
-} ;
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
 
 ?>
 
@@ -19,6 +24,30 @@ foreach($pizzas as $pizza){
 <html>
 
    <?php include './templates/header.php'; ?>
+   
+   <h4 class="center grey-text">Pizzas</h4>
+
+   <div class="container">
+      <div class="row">
+
+         <?php foreach($pizzas as $pizza){ ?>
+            
+            <div class="col s6 md3">
+               <div class="card z-depth-0">
+                  <div class="card-content center">
+                     <h6> <?php echo htmlspecialchars($pizza['title']); ?></h6>
+                     <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                  </div>
+                  <div class="card-action right-align">
+                     <a href="#" class="brand-text">More info</a>
+                  </div>
+               </div>
+            </div>
+
+         <?php } ?> 
+
+      </div>
+   </div>
 
    <?php include './templates/footer.php'; ?>
 
