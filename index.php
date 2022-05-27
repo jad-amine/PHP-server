842e7dd92ad647b261824836f84ffb7c97c80008
jad amine <jade1992amine@gmail.com>
2022-05-27 09:54:07 +0300

    Operators

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
 
-echo 'hello world'
-
+$products = [
+   ['name' => 'star', 'price' => 20],
+   ['name' => 'coin', 'price' => 10]
+];
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
+   <title>Apache Server</title>
 </head>
 <body>
-   <h1><?php echo 'hello world' ?></h1>
+   
+   <?php echo TRUE ?>
+   <?php echo FALSE ?>
+   <?php echo TRUE ?>
+
 </body>
 </html>
